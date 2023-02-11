751fb9d653857bf64c1b56a3358d585d1f8ec21c
ShalomShahzad <shalomshahzad33@gmail.com>
2023-02-11 10:09:18 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//7cf2e4f0
+
+const API_URL = "http://www.omdbapi.com?apikey=7cf2e4f0"
+
+const App = () =>{
+
+
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies("Spider-Man")
+    }, [])
+
+    return(
+        <div className="app">
+            <h1>SearchMovies</h1>
+        </div>
+    );
 }
 
-export default App;
+export default App
